65409da82e72057c4e576d68b4789d816e362298
Wayne Brown <wayne.brown@appdynamics.com>
2021-09-21 19:10:24 -0600

    Added loadgen Kubernetes files. Updated Docker build.

--- examples/docker_build.sh
+++ examples/docker_build.sh
@@ -15,18 +15,19 @@ while getopts 'pr:' flag; do
   esac
 done
 
-docker build --no-cache=true -t "$registry/teastore-base" ../utilities/tools.descartes.teastore.dockerbase/
-perl -i -pe's|.*FROM descartesresearch/|FROM '"$registry"'/|g' ../services/tools.descartes.teastore.*/Dockerfile
+# docker build --no-cache=true -t "$registry/teastore-base" ../utilities/tools.descartes.teastore.dockerbase/
+# perl -i -pe's|.*FROM descartesresearch/|FROM '"$registry"'/|g' ../services/tools.descartes.teastore.*/Dockerfile
 docker build -t "$registry/teastore-registry" ../services/tools.descartes.teastore.registry/
 docker build -t "$registry/teastore-persistence" ../services/tools.descartes.teastore.persistence/
 docker build -t "$registry/teastore-image" ../services/tools.descartes.teastore.image/
 docker build -t "$registry/teastore-webui" ../services/tools.descartes.teastore.webui/
 docker build -t "$registry/teastore-auth" ../services/tools.descartes.teastore.auth/
 docker build -t "$registry/teastore-recommender" ../services/tools.descartes.teastore.recommender/
+docker build -t "$registry/teastore-loadgen" ./jmeter/docker/
 perl -i -pe's|.*FROM '"$registry"'/|FROM descartesresearch/|g' ../services/tools.descartes.teastore.*/Dockerfile
 
 if [ "$push_flag" = 'true' ]; then
-  docker push "$registry/teastore-base"
+  # docker push "$registry/teastore-base"
   docker push "$registry/teastore-registry"
   docker push "$registry/teastore-persistence"
   docker push "$registry/teastore-image"
